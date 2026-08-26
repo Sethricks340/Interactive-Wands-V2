aa1c6dc85e6a7a7901586a409bf0fe59d7f50e0c
Sethricks340 <slricks340@byui.edu>
2026-08-26 14:49:03 -0600

    attempt update

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,6 @@ void loop() {
     
     // change color test
     digitalWrite(RED_PIN, HIGH);
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(BLUE_PIN, LOW);
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(BLUE_PIN, HIGH);
 }
