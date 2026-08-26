d85d20688b2090c56f8e24b620f329b45e793b02
Sethricks340 <slricks340@byui.edu>
2026-08-26 14:35:37 -0600

    attempt OTA update

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,6 @@ void loop() {
     
     // Green
     digitalWrite(RED_PIN, LOW);
-    digitalWrite(GREEN_PIN, LOW);
-    digitalWrite(BLUE_PIN, HIGH);
+    digitalWrite(GREEN_PIN, HIGH);
+    digitalWrite(BLUE_PIN, LOW);
 }
